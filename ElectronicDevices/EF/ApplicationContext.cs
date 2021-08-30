88e528d9c9b02ed04d49ab8ac84da42602fe6fa0
Veronika <veronika.ria.v@gmail.com>
2021-08-30 22:49:04 +0300

    Add ef core to project

---
+++ ElectronicDevices/EF/ApplicationContext.cs
@@ -0,0 +1,16 @@
+﻿using ElectronicDevices.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ElectronicDevices.EF
+{
+    public class ApplicationContext: DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options): base(options)
+        {
+        }
+
+        public DbSet<Device> Device { get; set; }
+        public DbSet<Kind> Kind { get; set; }
+
+    }
+}
--- ElectronicDevices/Startup.cs
+++ ElectronicDevices/Startup.cs
@@ -1,6 +1,8 @@
+using ElectronicDevices.EF;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -22,6 +24,9 @@ namespace ElectronicDevices
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationContext>(options =>
+                options.UseSqlServer(this.Configuration.GetConnectionString("DefaultConnection"))
+            );
             services.AddControllersWithViews();
         }
 
