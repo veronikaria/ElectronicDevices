dbc1173b36896b3a519366a174968137d0c4ff53
Veronika <veronika.ria.v@gmail.com>
2021-08-30 21:17:07 +0300

    Add device and kind model

---
+++ ElectronicDevices/Models/Device.cs
@@ -0,0 +1,16 @@
+﻿namespace ElectronicDevices.Models
+{
+    public class Device
+    {
+        public int DeviceId { get; set; }
+        public string Name { get; set; }
+        public string ShortDescription { get; set; }
+        public string LongDescription { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string ImageUrl { get; set; }
+        public string SmallImageUrl { get; set; }
+        public int KindId { get; set; }
+        public virtual Kind Kind { get; set; }
+    }
+}
---
+++ ElectronicDevices/Models/Kind.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace ElectronicDevices.Models
+{
+    public class Kind
+    {
+        public int KindId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Device> Devices { get; set; }
+    }
+}
