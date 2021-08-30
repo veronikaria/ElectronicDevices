6e18a7f9c340a9796678d06d4ec1132c940b4444
Veronika <veronika.ria.v@gmail.com>
2021-08-30 22:28:44 +0300

    Add device controller and list view

---
+++ ElectronicDevices/Controllers/DeviceController.cs
@@ -0,0 +1,26 @@
+﻿using ElectronicDevices.Interfaces;
+using ElectronicDevices.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ElectronicDevices.Controllers
+{
+    public class DeviceController: Controller
+    {
+        private readonly IDeviceRepository deviceRepository;
+        private readonly IKindRepository kindRepository;
+
+        public DeviceController(IDeviceRepository deviceRepository, IKindRepository kindRepository)
+        {
+            this.deviceRepository = deviceRepository;
+            this.kindRepository = kindRepository;
+        }
+
+        public IActionResult List() 
+        {
+            List<Device> devices = this.deviceRepository.Devices.ToList();
+            return View(devices);
+        }
+    }
+}
