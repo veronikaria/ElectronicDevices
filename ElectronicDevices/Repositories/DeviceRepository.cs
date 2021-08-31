6751222b81f7f32a4c19ee2c427b08525f979044
Veronika <veronika.ria.v@gmail.com>
2021-08-31 08:42:56 +0300

    Kind and device repositories implemented

---
+++ ElectronicDevices/Repositories/DeviceRepository.cs
@@ -0,0 +1,35 @@
+﻿using ElectronicDevices.EF;
+using ElectronicDevices.Interfaces;
+using ElectronicDevices.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ElectronicDevices.Repositories
+{
+    public class DeviceRepository : IDeviceRepository
+    {
+        private readonly ApplicationContext context;
+
+        public DeviceRepository(ApplicationContext context)
+        {
+            this.context = context;
+        }
+        public IEnumerable<Device> Devices 
+        {
+            get => this.context.Device.Include(dev => dev.Kind).ToList(); 
+            set => throw new NotImplementedException(); 
+        }
+
+        public Device GetDeviceById(int id)
+        {
+            return this.context.Device.FirstOrDefault(dev => dev.DeviceId==id);
+        }
+
+        public Device GetDeviceByName(string name)
+        {
+            return this.context.Device.FirstOrDefault(dev => dev.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
---
+++ ElectronicDevices/Repositories/KindRepository.cs
@@ -0,0 +1,23 @@
+﻿using ElectronicDevices.EF;
+using ElectronicDevices.Interfaces;
+using ElectronicDevices.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ElectronicDevices.Repositories
+{
+    public class KindRepository : IKindRepository
+    {
+        private readonly ApplicationContext context;
+
+        public KindRepository(ApplicationContext context)
+        {
+            this.context = context;
+        }
+        public IEnumerable<Kind> Kinds 
+        {
+            get => this.context.Kind.ToList(); 
+            set => throw new System.NotImplementedException(); 
+        }
+    }
+}
